d211102e07f38f2b8b9df7e008dd8301cedfd0df
rohith28pv <rohith28.pv@gmail.com>
2026-09-23 11:31:22 +0000

    feat: integrated express server wth conditional test protection

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello World! My first Dockersized DevOps web application is running.')
+});
+
 function add(a, b) {
-    return a + b;
+	return a + b;
 }
+
+if (require.main === module) {
+	app.listen(PORT, () => {
+		console.log('Server running on port ${PORT}');
+	});
+}
+
 module.exports = { add };
+
